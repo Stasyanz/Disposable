5357c1556fc629528681335f59f332d017932f06
dev <stasyanzayac@gmail.com>
2022-07-22 20:29:36 +0300

    add generate_id() function instead of uuid4()

--- src/core/services.py
+++ src/core/services.py
@@ -1,5 +1,11 @@
 from src.core.config import fqdn, schema, config
+from secrets import token_urlsafe
 
 
 def create_link(message_id: str):
     return f"{schema}{fqdn}:{config.get('SERVER_PORT')}/{message_id}"
+
+
+def generate_id():
+    """Generate message.id"""
+    return token_urlsafe(64)
