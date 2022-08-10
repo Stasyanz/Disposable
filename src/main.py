aaf9c3ac94d985150e4ab7e0d444cb1658fd42b4
dev <stasyanzayac@gmail.com>
2022-08-10 19:16:33 +0300

    add copy button to copy message link to the clipboard

--- src/main.py
+++ src/main.py
@@ -2,13 +2,17 @@ from fastapi import FastAPI
 import uvicorn
 
 from src.db.base import database
-from src.endpoints import messages, pages
 from src.core.config import config
+from src.endpoints import messages, pages
+
+
+def setup_routers(app: FastAPI):
+    app.include_router(messages.router)
+    app.include_router(pages.router)
+
 
 app = FastAPI(title="Disposable Messages")
-
-app.include_router(messages.router)
-app.include_router(pages.router)
+setup_routers(app)
 
 
 @app.on_event("startup")
